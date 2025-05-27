14d6bfc2281e9bd27b39d53a289d94d4bebf252e
giomara-larraga <giomara.lm@gmail.com>
2025-05-27 18:54:10 +0300

    refactor: remove dark mode configuration and safelist from Tailwind CSS config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,7 @@
-import { fontFamily } from "tailwindcss";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	darkMode: ["class"],
 	content: ["./src/**/*.{html,js,svelte,ts}"],
-	safelist: ["dark"],
 	theme: {
 		container: {
 			center: true,
