1cc65cb92e6b80ef0da6c24bde8e3a58fb0ac2db
giomara-larraga <giomara.lm@gmail.com>
2025-05-27 15:59:50 +0300

    refactor: remove unused components and styles, update Button story

--- src/stories/Button.stories.svelte
+++ src/stories/Button.stories.svelte
@@ -1,31 +1,18 @@
 <script module>
-  import { defineMeta } from '@storybook/addon-svelte-csf';
-  import Button from './Button.svelte';
-  import { fn } from '@storybook/test';
+import { defineMeta } from '@storybook/addon-svelte-csf';
 
-  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-  const { Story } = defineMeta({
-    title: 'Example/Button',
+import { Button } from '../lib/components/ui/button/index.js';
+	import { Label } from 'bits-ui';
+
+ const { Story } = defineMeta({
+    title: 'MyComponent',
     component: Button,
-    tags: ['autodocs'],
-    argTypes: {
-      backgroundColor: { control: 'color' },
-      size: {
-        control: { type: 'select' },
-        options: ['small', 'medium', 'large'],
-      },
-    },
-    args: {
-      onClick: fn(),
-    }
+
   });
 </script>
 
-<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
-<Story name="Primary" args={{ primary: true, label: 'Button' }} />
-
-<Story name="Secondary" args={{ label: 'Button' }} />
-
-<Story name="Large" args={{ size: 'large', label: 'Button' }} />
-
-<Story name="Small" args={{ size: 'small', label: 'Button' }} />
+<Story name="Default" args={{ variant: "default", size: "default" }} tags={['autodocs']}>
+  {#snippet children(args)}
+    <Button {...args}>Reactive component</Button>
+  {/snippet}
+</Story>
